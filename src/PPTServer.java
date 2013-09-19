d57bcc97db4f352ace1fd3af5589407c6a941e10
yimun <755213779@qq.com>
2013-09-19 11:10:42 +0800

    功能已实现

--- src/PPTServer.java
+++ src/PPTServer.java
@@ -1,10 +1,9 @@
-
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,28 +12,25 @@ public class PPTServer {
 	private final static int LEFT = 2;
 	private final static int SHIFTF5 = 0;
 	private final static int ESC = 3;
-
 	private static int key;
-	// 注意这里用的输入输出流的对象
-	private static ObjectInputStream fromClient;
-	private static ObjectOutputStream fromServer;
 
 	public static void main(String[] args) throws IOException,
 			ClassNotFoundException, AWTException, InterruptedException {
 		ServerSocket sSocket = new ServerSocket(2011);
 		System.out.println("waiting a connection from the client");
 		Robot robot = new Robot();
-		Socket sock = sSocket.accept();
-		System.out.println("recv a connection");
-		fromClient = new ObjectInputStream(sock.getInputStream());
-		fromServer = new ObjectOutputStream(sock.getOutputStream());
+		
 		do {
-			Choices choice = (Choices) fromClient.readObject();
-			System.out.println("the flag is " + choice.getKey());
-			key = choice.getKey();
-			switch (key) {
+			Socket socket = sSocket.accept();
+			System.out.println("recv a connection");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			String str = in.readLine();
+			System.out.println("the flag is " + str);
 
-			case SHIFTF5:
+			switch (str) {
+
+			case "shift5":
 				robot.keyPress(KeyEvent.VK_SHIFT);
 				Thread.sleep(20);
 				robot.keyPress(KeyEvent.VK_F5);
@@ -44,21 +40,21 @@ public class PPTServer {
 				Thread.sleep(10);
 				break;
 
-			case LEFT:
+			case "left":
 				robot.keyPress(KeyEvent.VK_LEFT);
 				Thread.sleep(10);
 				robot.keyRelease(KeyEvent.VK_LEFT);
 				Thread.sleep(10);
 				break;
 
-			case RIGHT:
+			case "right":
 				robot.keyPress(KeyEvent.VK_RIGHT);
 				Thread.sleep(10);
 				robot.keyRelease(KeyEvent.VK_RIGHT);
 				Thread.sleep(10);
 				break;
 
-			case ESC:
+			case "esc":
 				robot.keyPress(KeyEvent.VK_ESCAPE);
 				Thread.sleep(10);
 				robot.keyPress(KeyEvent.VK_ESCAPE);
@@ -68,11 +64,11 @@ public class PPTServer {
 			default:
 				break;
 			}
+			in.close();
+			socket.close();
+	
 		} while (key != -1);
 		System.out.println("exit the app");
-		fromClient.close();
-		fromServer.close();
-		sock.close();
-		sSocket.close();
+		
 	}
 }
